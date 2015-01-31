b6a31e84cc5c88b0e55728fbf4ddc64fb7472b05
Kip Gebhardt <kgebhardt@kqed.org>
2015-01-30 16:15:30 -0800

    Adding unit test for plugins

--- test/index.js
+++ test/index.js
@@ -6,7 +6,7 @@ var test = require('tape'),
 // lastBuildDate will always be this value
 var mockdate = require('mockdate').set('Wed, 31 Dec 2014 00:00:01 GMT');
 
-test('braid feed from file', function(t) {
+test('braid feed from file without plugins', function(t) {
     t.plan(1);
     var feeds = {};
     feeds.sample_feed = require("./input_files/sample_feed").feed;
@@ -27,6 +27,26 @@ test('braid feed from file', function(t) {
     });
 });
 
+test('braid feed from file with plugins', function(t) {
+    t.plan(1);
+    var feeds = {};
+    feeds.sample_feed = require("./input_files/sample_feed_plugins").feed;
+    var braider_options = {
+        feeds           : feeds,
+        indent          : "    ",
+        date_sort_order : "desc"
+    };
+    var rss_braider = RssBraider.createClient(braider_options);
+
+    rss_braider.processFeed('sample_feed', 'rss', function(err, data){
+        if (err) {
+            return t.fail(err);
+        }
+        // console.log(data);
+        t.equal(data, expectedOutput.fileFeedOutputPlugins);
+
+    });
+});
 test('deduplicate feed from file', function(t) {
     t.plan(1);
     var feeds = {};
@@ -43,7 +63,7 @@ test('deduplicate feed from file', function(t) {
             return t.fail(err);
         }
         // console.log(data);
-        t.equal(data, expectedOutput.fileFeedOutput);
+        t.equal(data, expectedOutput.fileFeedOutputPlugins);
     });
 });
 
